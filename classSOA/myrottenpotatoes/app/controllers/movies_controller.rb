7b32f7ae660dd4c984d6119d3e0a090fc92bd11e
saasbook <saasbook@saasbook.(none)>
2013-10-21 20:47:07 +0800

    week 6

--- classSOA/myrottenpotatoes/app/controllers/movies_controller.rb
+++ classSOA/myrottenpotatoes/app/controllers/movies_controller.rb
@@ -12,5 +12,47 @@ class MoviesController < ApplicationController
   # will render app/views/movies/show.html.haml by default
 end
 
+  # as a reminder, here is the original 'new' method:
+def new
+  @movie = Movie.new
+end
+
+# in movies_controller.rb
+# replaces the 'create' method in controller:
+def create
+  @movie = Movie.new(params[:movie])
+  if @movie.save
+    flash[:notice] = "#{@movie.title} was successfully created."
+    redirect_to movies_path
+  else
+    render 'new' # note, 'new' template can access @movie's field values!
+  end
+end
+
+# in movies_controller.rb
+ 
+def edit
+  @movie = Movie.find params[:id]
+end
+ 
+# replaces the 'update' method in controller:
+def update
+  @movie = Movie.find params[:id]
+  if @movie.update_attributes(params[:movie])
+    flash[:notice] = "#{@movie.title} was successfully updated."
+    redirect_to movie_path(@movie)
+  else
+    render 'edit' # note, 'edit' template can access @movie's field values!
+  end
+end
+
+def destroy
+  @movie = Movie.find(params[:id])
+  @movie.destroy
+  flash[:notice] = "Movie '#{@movie.title}' deleted."
+  redirect_to movies_path
+end
+
+
 end
 
